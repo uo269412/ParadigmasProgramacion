794e111049febaa1c1be3b8181726716bd7c9480
Javier Carrillo <60443414+uo269412@users.noreply.github.com>
2023-03-15 11:41:59 +0100

    Add files via upload

---
+++ lab01_LinkedList/lab01.list.linked/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using linked.list;
+
+namespace use.linked.list
+{
+    class Program
+    {
+        static void Main()
+        {
+            List l = new List();
+            l.Add(1);
+            Console.WriteLine(l.NumberOfElements); //1
+            l.Add(2);
+            Console.WriteLine(l.NumberOfElements); //2
+            l.Add(3);
+            Console.WriteLine(l.NumberOfElements); //3
+            Console.WriteLine(l.GetElement(0)); //1
+            Console.WriteLine(l.GetElement(1)); //2
+            Console.WriteLine(l.GetElement(2)); //3
+            l.Remove(1);
+            Console.WriteLine(l.GetElement(0)); //2
+            Console.WriteLine(l.NumberOfElements); //2
+            l.Remove(2);
+            Console.WriteLine(l.GetElement(0)); //3
+            Console.WriteLine(l.NumberOfElements); //1
+            l.Remove(0);
+        }
+    }
+}
---
+++ lab01_LinkedList/linked.list/List.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linked.list
+{
+    public class Node
+    {
+        public Node Next { get; set; }
+        public int Content { get; set; }
+        public Node(int content)
+        {
+            Content = content;
+        }
+    }
+    public class List
+    {
+        private Node First;
+        public int NumberOfElements { get; set; }
+
+        public void Add(int number)
+        {
+            Node AdditionNode = new Node(number);
+            if (NumberOfElements == 0)
+            {
+                First = AdditionNode;
+            } else
+            {
+                Node PreviousNode = First;
+                while (PreviousNode.Next != null )
+                {
+                    PreviousNode = PreviousNode.Next;
+                }
+                PreviousNode.Next = AdditionNode;
+            }
+            NumberOfElements++;
+        }
+
+        public void Remove(int value)
+        {
+            Node PriorNode = First;
+            Node FoundNode = First.Next;
+
+            while (FoundNode != null)
+            {
+                if (PriorNode.Content == value)
+                {
+                    First = FoundNode;
+                    NumberOfElements--;
+                    break;
+                }
+                else if (FoundNode.Content == value)
+                {
+                    PriorNode.Next = FoundNode.Next;
+                    NumberOfElements--;
+                    break;
+                }
+                else
+                {
+                    PriorNode = FoundNode;
+                    FoundNode = FoundNode.Next;
+                }
+            }
+        }
+
+        public int GetElement(int position)
+        {
+            Node NodeToProcess = First;
+            for (int i = 0; i < position; i++)
+            {
+                NodeToProcess = NodeToProcess.Next;
+            }
+            return NodeToProcess.Content;
+        }
+    }
+}
